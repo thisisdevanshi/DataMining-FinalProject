a8df8299c5e76379a14076782d875be672c1247b
Devanshi Verma <devanshi@uchicago.edu>
2020-08-27 23:53:38 -0500

    adding ensemble

---
+++ Code/Evaluation_Metrics.py
@@ -0,0 +1,35 @@
+"""Metrics to compute the model performance."""
+
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import make_scorer
+
+
+def dcg_score(y_true, y_score, k=5):
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order[:k])
+
+    gain = 2 ** y_true - 1
+
+    discounts = np.log2(np.arange(len(y_true)) + 2)
+    return np.sum(gain / discounts)
+
+
+def ndcg_score(ground_truth, predictions, k=5):
+    lb = LabelBinarizer()
+    lb.fit(range(len(predictions) + 1))
+    T = lb.transform(ground_truth)
+
+    scores = []
+
+    # Iterate over each y_true and compute the DCG score
+    for y_true, y_score in zip(T, predictions):
+        actual = dcg_score(y_true, y_score, k)
+        best = dcg_score(y_true, y_true, k)
+        score = float(actual) / float(best)
+        scores.append(score)
+
+    return np.mean(scores)
+
+
+ndcg_scorer = make_scorer(ndcg_score, needs_proba=True, k=5)
